35f9627f66c64e7e8ddbd3501d82388d1075df3d
Nils Hartmann <nils@nilshartmann.net>
2026-05-06 20:59:30 +0200

    06: Ausblick Externalized

--- src/main/java/nh/demo/plantify/billing/Invoice.java
+++ src/main/java/nh/demo/plantify/billing/Invoice.java
@@ -1,11 +1,20 @@
 package nh.demo.plantify.billing;
 
+import org.springframework.modulith.events.Externalized;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.util.List;
 import java.util.UUID;
 
+// Normalerweise würden hier natürlich ein "echtes" Event nehmen
+// und nicht unsere "Entity"
+
+// Demo ist (zu) komplex:
+// -> nur erklären
+// -> Publish mit "normalem" applicationEventPublisher
+@Externalized(target = "invoices::#{#this.ownerId()}")
 record Invoice(
     UUID id,
     LocalDateTime createdAt,
