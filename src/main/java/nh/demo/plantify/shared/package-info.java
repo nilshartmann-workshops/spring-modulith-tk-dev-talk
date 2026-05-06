de4fb38c0e394567c5d09e942b583553e1225abd
Nils Hartmann <nils@nilshartmann.net>
2026-05-06 20:13:38 +0200

    04d: OPEN Module (nur, wenn Zeit ist)

---
+++ src/main/java/nh/demo/plantify/shared/package-info.java
@@ -0,0 +1,4 @@
+@ApplicationModule(displayName = "Legacy stuff. Todo: Refactoring!", type = ApplicationModule.Type.OPEN)
+package nh.demo.plantify.shared;
+
+import org.springframework.modulith.ApplicationModule;
