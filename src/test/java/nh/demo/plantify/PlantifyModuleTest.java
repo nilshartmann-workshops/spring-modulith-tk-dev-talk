6d458989caed71af3adc0054115acab0b45ccb49
Nils Hartmann <nils@nilshartmann.net>
2026-05-06 21:01:29 +0200

    10: Review Architektur mit uses, depends on und listens

--- src/test/java/nh/demo/plantify/PlantifyModuleTest.java
+++ src/test/java/nh/demo/plantify/PlantifyModuleTest.java
@@ -30,6 +30,15 @@ public class PlantifyModuleTest {
         var modules = ApplicationModules
             .of(PlantifyApplication.class);
 
+        // Nochmal Doku erzeugen
+        //  - Wir sehen jetzt:
+        //    - depends on: verwendet Klassen
+        //    - uses: Verwendet Services
+        //    - listens to: Verwendet Events
+        //
+        // Das können wir gleich für die Tests benutzen
+
+
         new Documenter(modules)
             .writeDocumentation();
 
